4884311c3bb2e0f61efa54119fe255783ce9d565
awaitman <awaitman@umich.edu>
2019-12-19 00:29:54 -0500

    fixed

--- script.js
+++ script.js
@@ -2,4 +2,20 @@ $(document).ready( function() {
 
   console.log("Ready!");
 
-});
+  document.getElementById("us").innerHTML = new Date();
+
+  var usaTime = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
+  usaTime = new Date(usaTime);
+  document.getElementById("us").innerHTML = usaTime.toLocaleString();
+
+
+  usaTime.setHours(usaTime.getHours() + 6);
+  document.getElementById("madrid").innerHTML = usaTime.toLocaleString();
+
+
+
+  var aestTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane"});
+  aestTime = new Date(aestTime);
+  document.getElementById("aus").innerHTML = aestTime.toLocaleString();
+
+})
