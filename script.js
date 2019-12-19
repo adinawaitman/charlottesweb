4d01c7604be66c6d66adc31ffa2a45a3693890f4
awaitman <awaitman@umich.edu>
2019-12-18 23:04:44 -0500

    updates

--- script.js
+++ script.js
@@ -1,41 +1,5 @@
-// on ready, make the initial call and append to HTML
 $(document).ready( function() {
 
   console.log("Ready!");
 
-  // add keydown router
-  $(window).keydown(keydownRouter);
-
-  addToCarousel();
-
 });
-
-
-
-function addToCarousel() {
-  var photos = {"img/DSC_3600.JPG",
-
-  };
-
-  $.each(photos, function(i, photo)) {
-        var src = photo;
-        let active;
-        if (i == 0)
-          active = " active";
-        else
-            active = '';
-
-        // Create the img html and set the src attribute to our URL
-        let imgHtml = $("<img/>").attr("src", src);
-
-        // add the src and photo id to dict mapper for purpose of getting ID
-        let link = '<img src="' + src + '"' + '>';
-        idMapper[link] = photo.id;
-
-        // Create the .item div and insert the img html into it
-        let itemDivHtml = $("<div class='item" + active + "' width='460' height='345'/>").append(imgHtml);
-
-        // Insert the .item div into the .carousel-inner div
-        $(".carousel-inner").append(itemDivHtml);
-  })
-}
